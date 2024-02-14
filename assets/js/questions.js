b9cde5e97bcbfbe26939327e790bdcd51fd61f70
emily <emilycassel77@gmail.com>
2024-02-14 17:56:52 -0500

    comments

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,61 +1,61 @@
-export const questions = [
+const questions = [
     {
-        question: ("How many stomachs does a cow have?"),
-        options: ("one", "two", "three", "four", "five"),
-        correct: ("four") 
+        question: "How many stomachs does a cow have?",
+        options: ["one", "two", "three", "four", "five"],
+        correct: "four" 
     },
 
     {
         question: ("Which large cat lives in prides?"),
-        options: ("tiger", "lion", "cheetah", "panther", "jaguar"),
-        correct: ("lion") 
+        options: ["tiger", "lion", "cheetah", "panther", "jaguar"],
+        correct: "lion"
     },
 
     {
         question: ("Which animal plays dead when scared?"),
-        options: ("opposum", "raccoon", "fox", "ground hog", "muskrat"),
+        options: ["opposum", "raccoon", "fox", "ground hog", "muskrat"],
         correct: ("opposum") 
     },
 
     {
         question: ("Which bird can fly?"),
-        options: ("emu", "chicken", "penguin", "hawk", "ostrich"),
+        options: ["emu", "chicken", "penguin", "hawk", "ostrich"],
         correct: ("hawk") 
     },
 
     {
         question: ("Which animal lives in North America?"),
-        options: ("tiger", "emu", "panda bear", "mountain lion", "elephant"),
+        options: ["tiger", "emu", "panda bear", "mountain lion", "elephant"],
         correct: ("mountain lion") 
     },
 
     {
         question: ("Which animal is used for its wool?"),
-        options: ("shark", "geese", "pig", "sheep", "llama"),
+        options: ["shark", "geese", "pig", "sheep", "llama"],
         correct: ("sheep") 
     },
 
     {
         question: ("Which animal is an omnivore?"),
-        options: ("hawk", "killer whale", "seal", "horse", "bear"),
+        options: ["hawk", "killer whale", "seal", "horse", "bear"],
         correct: ("bear") 
     },
 
     {
         question: ("Which animal builds its nest in dams?"),
-        options: ("platypus", "salimander", "raccoon", "beaver", "otter"),
+        options: ["platypus", "salimander", "raccoon", "beaver", "otter"],
         correct: ("beaver") 
     },
 
     {
         question: ("Which animal lays eggs even though it is a mammal?"),
-        options: ("lizard", "chicken", "bear", "platypus", "skunk"),
+        options:  ["lizard", "chicken", "bear", "platypus", "skunk"],
         correct: ("platypus") 
     },
 
     {
         question: ("Which animal uses rocks to open clams and oysters?"),
-        options: ("skunk", "seal", "otter", "rabbit", "bear"),
+        options: ["skunk", "seal", "otter", "rabbit", "bear"],
         correct: ("otter") 
     },
 ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
-const lq = require("./questions.js");
+//const quizQues = require("./questions");
 
+
+console.log(questions)
 //start with varibales 
 //know you are going to have to query select the button 
 //remmeber for the button need an event listener and reference button 
@@ -12,15 +14,16 @@ let currentQuestion = 0; //this is the start to the array of objects in the ques
 //begin quiz
 
 
-function showQuestion(){};
+quesOptContainer.classList.add("hide");
+
 
 function beginQuiz(){
-    let begbtn = document.getElementById("begbtn");
-
     begbtn.classList.add("hide");
-    questions.classList.remove("hide");
+    quesOptContainer.classList.remove("hide");
     showQuestion(questions[currentQuestionIndex]);
 }
+
+
 
 function showQuestion(question){
    ques.innerText = questions.question
@@ -62,6 +65,9 @@ function stopQuiz(){
 
 
 function beginTimer(){
+    
+    beginQuiz();
+
     let timeCountElement = document.getElementById("timeCount"); //targeting the time count element so we can use javascript on
     //timeCountElement is a variable thats value is an element
 
@@ -71,7 +77,10 @@ function beginTimer(){
     //when it runs out want to see time up(timeLeft = 0)
     //quiz stops
 
+    
+
     timeCountElement.textContent = "Time Left: " + timeLeft + " seconds";//the .textContent is a property fo the timeCountElement
+
 
     if(timeLeft === 0){
         timeCountElement.textContent = "Quiz Over"
@@ -99,18 +108,24 @@ console.log(timeLeft)
 //start quiz and start timer will be the same
 //getElementById is a method
 // use to build the line below document.getElementById("begbtn")
+let begbtn = document.getElementById("begbtn");
 //assigning the btn so it can be used later such as in the line below
 begbtn.addEventListener("click", beginTimer);//adding . to the element and now the element can be used to complete the job of beginQuiz
 
 
-let nxtbtn = document.getElementById("nxtbtn");
-nxtbtn.addEventListener("click", nextQuestion);
+
 
 
 let ansbtn = document.getElementById("ansbtn");
 
 
 document.getElementById("ques").innerText = questions.question
+
+
+
+
+let quesOptContainer =document.getElementById("quesOptContainer"); 
+
 
 //let person = ("emily") is the same as 
 //let begbtn = document.getElementById("begbtn") when the code runs document.getElementById("begbtn") has the exact same as the string ("emily") only the document.getElementById("begbtn") is a differnet type of string that can do different types of things to 
@@ -124,10 +139,10 @@ document.getElementById("ques").innerText = questions.question
 
 
 
-console.log(document.getElementById("begbtn"))
+console.log(document.getElementById("begbtn"));
 
-console.log(document.getElementById("nxtbtn"))
 
+console.log(quesOptContainer);
 
 
 
